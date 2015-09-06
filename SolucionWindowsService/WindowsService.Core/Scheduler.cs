3d14128dfa33d527f5f2b35cae8fbcd35f8cf2e4
EDSON <edsonflorespalma@gmail.com>
2015-09-06 02:09:16 -0500

    Initial Creation

---
+++ SolucionWindowsService/WindowsService.Core/Scheduler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService.Core
+{
+    public partial class Scheduler : ServiceBase
+    {
+        public Scheduler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
